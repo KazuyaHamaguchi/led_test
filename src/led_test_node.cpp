b189b2e87e5ef800c44df493e5a3e0b528192946
Kazuya Hamaguchi <kzhamachan@gmail.com>
2017-09-14 22:07:40 +0900

    Complete led_test_node.cpp

--- src/led_test_node.cpp
+++ src/led_test_node.cpp
@@ -6,13 +6,15 @@ int main(int argc, char **argv)
 	int pi = pigpio_start(0, 0);
 	ros::init(argc, argv, "led_test_node");
 	set_mode(pi, 22, PI_OUTPUT);
-	ROS_INFO("set_mode(0, 22, PI_OUTPUT);");
-	gpio_write(pi, 22, 1);
-	ROS_INFO("gpio_write(0, 22, 1);");
-	time_sleep(1);
-	ROS_INFO("time_sleep(1);");
-	gpio_write(pi, 22, 0);
-	ROS_INFO("gpio_write(0, 22, 0);");
+	while(1)
+	{
+		gpio_write(pi, 22, 1);
+		ROS_INFO("LED ON");
+		time_sleep(1);
+		gpio_write(pi, 22, 0);
+		ROS_INFO("LED OFF");
+		time_sleep(1);
+	}
 	pigpio_stop(pi);
 	return 0;	
 }
